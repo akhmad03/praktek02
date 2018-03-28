272850eced60d48c2b8b6c74097aabb20b45250e
akhmad03 <37820079+akhmad03@users.noreply.github.com>
2018-03-28 15:58:08 +0800

    instance object

--- src/praktek02/tvaksi.java
+++ src/praktek02/tvaksi.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package praktek02;
 
-/**
- *
- * @author windows 10
- */
 public class tvaksi {
-    
+    public static void main(String[] args) {
+        tv tv1 = new tv();
+    }
 }
