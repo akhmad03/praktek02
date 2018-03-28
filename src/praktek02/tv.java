55fb038f8434f1bf52128c7f61d7c518697a80e0
akhmad03 <37820079+akhmad03@users.noreply.github.com>
2018-03-28 15:44:42 +0800

    membuat attribute dan behavior

--- src/praktek02/tv.java
+++ src/praktek02/tv.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package praktek02;
 
-/**
- *
- * @author windows 10
- */
+
 public class tv {
+    Integer hargatv;
+    Integer jumlahtv;
+    
+   void nonton() {
+        System.out.println(" hargatv : "+hargatv);
+        System.out.println(" jumlahtv : "+jumlahtv);
+        
+    }
     
 }
