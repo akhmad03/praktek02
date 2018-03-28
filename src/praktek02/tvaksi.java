88fef4cf417347d2f460a0205f85f6a4fb6214f9
akhmad03 <37820079+akhmad03@users.noreply.github.com>
2018-03-28 16:01:14 +0800

    mengisi nilai attribute

--- src/praktek02/tvaksi.java
+++ src/praktek02/tvaksi.java
@@ -4,5 +4,7 @@ package praktek02;
 public class tvaksi {
     public static void main(String[] args) {
         tv tv1 = new tv();
+        tv1.hargatv = 15000000;
+        tv1.jumlahtv= 10;
     }
 }
